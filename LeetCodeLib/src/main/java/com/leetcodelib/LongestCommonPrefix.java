5b15658cdb01db7e4288b3b40755af3d01642b20
Administrator <1021864784@qq.com>
2018-08-19 19:56:29 +0800

    添加14题  最长公共前缀解答

---
+++ LeetCodeLib/src/main/java/com/leetcodelib/LongestCommonPrefix.java
@@ -0,0 +1,62 @@
+package com.leetcodelib;
+
+/**
+ * 14. 最长公共前缀
+ * https://leetcode-cn.com/problems/longest-common-prefix/description/
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * <p>
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 示例 2:
+ * <p>
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * 说明:
+ * <p>
+ * 所有输入只包含小写字母 a-z 。
+ */
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix("flower", "flow", "flight"));
+        System.out.println(longestCommonPrefix("flower", "flow", "floght"));
+        System.out.println(longestCommonPrefix("asdfghjk", "asdfyui", "asdfeqwr"));
+    }
+
+
+    public static String longestCommonPrefix(String... strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        if (strs.length == 1) {
+            return strs[0];
+        }
+        int lengthHead = Integer.MAX_VALUE;
+        //前缀最大长度也就取决于最短的那个单词
+        for (String str : strs) {
+            lengthHead = Math.min(lengthHead, str.length());
+        }
+        StringBuilder builder = new StringBuilder();
+
+        //第一个单词
+        String strFirst = strs[0];
+
+        for (int i = 0; i < lengthHead; i++) {
+            for (int i1 = 1; i1 < strs.length; i1++) {
+                //当前要对比的单词
+                String str = strs[i1];
+                if (strFirst.charAt(i) != str.charAt(i)) {
+                    //这里匹配不上了,说明查找结束,返回结果
+                    return builder.toString();
+                }
+            }
+            builder.append(strFirst.charAt(i));
+        }
+
+        return builder.toString();
+    }
+}
--- LeetCodeLib/src/main/java/com/leetcodelib/ReverseInteger.java
+++ LeetCodeLib/src/main/java/com/leetcodelib/ReverseInteger.java
@@ -1,39 +1,45 @@
-package com.leetcodelib;
-
-/**
- * 7. 反转整数
- * 题目地址:https://leetcode-cn.com/problems/reverse-integer/description/
- * <p>
- * 给定一个 32 位有符号整数，将整数中的数字进行反转。
- * <p>
- * 示例 1:
- * <p>
- * 输入: 123
- * 输出: 321
- * 示例 2:
- * <p>
- * 输入: -123
- * 输出: -321
- * 示例 3:
- * <p>
- * 输入: 120
- * 输出: 21
- * 注意:
- * <p>
- * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。根据这个假设，如果反转后的整数溢出，则返回 0。
- */
-public class ReverseInteger {
-    public static void main(String[] args) {
-
-    }
-
-    public int reverse(int x) {
-        String valueOf = String.valueOf(x);
-        String result = "";
-
-        for (int length = valueOf.length(); length > 0; length--) {
-//            result+=valueOf.(length);
-        }
-        return 0;
-    }
-}
+//package com.leetcodelib;
+//
+///**
+// * 7. 反转整数
+// * 题目地址:https://leetcode-cn.com/problems/reverse-integer/description/
+// * <p>
+// * 给定一个 32 位有符号整数，将整数中的数字进行反转。
+// * <p>
+// * 示例 1:
+// * <p>
+// * 输入: 123
+// * 输出: 321
+// * 示例 2:
+// * <p>
+// * 输入: -123
+// * 输出: -321
+// * 示例 3:
+// * <p>
+// * 输入: 120
+// * 输出: 21
+// * 注意:
+// * <p>
+// * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。根据这个假设，如果反转后的整数溢出，则返回 0。
+// */
+//public class ReverseInteger {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    public int reverse(int x) {
+//        boolean downZero = x < 0;
+////        Integer.MAX_VALUE
+//        int b = Math.abs(x);
+//        for (int i = 32; i > 0; i--) {
+////            b/i
+//        }
+//
+//        int result = 1;
+//        while (x / 10 != 0) {
+//            result*
+//        }
+////        x
+//        return 0;
+//    }
+//}
