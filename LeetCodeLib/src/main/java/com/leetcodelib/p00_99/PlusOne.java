18837b0266f2126088b2c6a88c9d95d98b435197
pengjunjing <1021864784@qq.com>
2019-11-08 15:05:39 +0800

    模拟加一

--- LeetCodeLib/src/main/java/com/leetcodelib/p00_99/PlusOne.java
+++ LeetCodeLib/src/main/java/com/leetcodelib/p00_99/PlusOne.java
@@ -1,63 +1,69 @@
-//package com.leetcodelib;
-//
-///**
-// * https://leetcode-cn.com/problems/plus-one/description/
-// * 66. 加一
-// * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
-// * <p>
-// * 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
-// * <p>
-// * 你可以假设除了整数 0 之外，这个整数不会以零开头。
-// * <p>
-// * 示例 1:
-// * <p>
-// * 输入: [1,2,3]
-// * 输出: [1,2,4]
-// * 解释: 输入数组表示数字 123。
-// * 示例 2:
-// * <p>
-// * 输入: [4,3,2,1]
-// * 输出: [4,3,2,2]
-// * 解释: 输入数组表示数字 4321。
-// */
-//public class PlusOne {
-//    public static void main(String[] args) {
-////        int[] ints = plusOne(new int[]{1, 2, 3});
-////        int[] ints = plusOne(new int[]{9});
-//        int[] ints = plusOne(new int[]{9,9});
-//        for (int anInt : ints) {
-//            System.out.println(anInt);
-//        }
-//    }
-//
-//    public static int[] plusOne(int[] digits) {
-////        if (digits.length == 1) {
-////            digits[0]++;
-////            return digits;
-////        }
-//        return plusOne(digits, digits.length - 1);
-//    }
-//
-//    public static int[] plusOne(int[] digits, int tag) {
-//        if (digits[0] == 9 && tag == 0) {
-//            digits[0] = 0;
-//            int[] digits2 = new int[digits.length + 1];
-//            for (int i = 0; i < digits.length; i++) {
-//                digits2[i + 1] = digits[i];
-//            }
-//            digits2[0] = 1;
-//            return plusOne(digits2, 1);
-//        }
-//        for (int i = 0; i < digits.length; i++) {
-//            if (i == tag) {
-//                if (digits[i] == 9) {
-//                    digits[i] = 0;
-//                    plusOne(digits, tag - 1);
-//                } else {
-//                    digits[i] = digits[i] + 1;
-//                }
-//            }
-//        }
-//        return digits;
-//    }
-//}
+package com.leetcodelib;
+
+import java.util.ArrayList;
+
+/**
+ * https://leetcode-cn.com/problems/plus-one/description/
+ * 66. 加一
+ * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+ * <p>
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+ * <p>
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3]
+ * 输出: [1,2,4]
+ * 解释: 输入数组表示数字 123。
+ * 示例 2:
+ * <p>
+ * 输入: [4,3,2,1]
+ * 输出: [4,3,2,2]
+ * 解释: 输入数组表示数字 4321。
+ */
+public class PlusOne {
+    public static void main(String[] args) {
+//        int[] ints = plusOne(new int[]{1, 2, 3});
+//        int[] ints = plusOne(new int[]{9});
+//        int[] ints = plusOne(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0});
+        int[] ints = plusOne(new int[]{9});
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+    }
+
+    /**
+     * 思路:
+     * 数组可能很大很大,所以不应该转为int 或者long的形式自增1再转为数组,如果这样做可能会溢出.
+     * 考虑直接暴力进行解题
+     */
+    public static int[] plusOne(int[] digits) {
+
+        int addIndex = digits.length - 1;//自增位的索引,从最后一位考虑自增
+
+        while (true) {
+            if (digits[addIndex] == 9) {
+                //自增位是9,需要进位
+                digits[addIndex] = 0;
+                addIndex--;
+                if (addIndex == -1) {
+                    //都已经加到最前面了,需要数组扩容一位,然后旧内容复制到新数组上
+                    int[] newDigits = new int[digits.length + 1];
+                    newDigits[0] = 1;
+                    for (int i = 0; i < newDigits.length - 1; i++) {
+                        newDigits[i + 1] = digits[i];
+                    }
+                    digits = newDigits;
+                    break;
+                }
+            } else {
+                //自增完成
+                digits[addIndex]++;
+                break;
+            }
+        }
+        return digits;
+    }
+
+}
